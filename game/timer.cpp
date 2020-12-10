ce4dfe9ec1848098589dea4ebaedcb92f43838b3
Grayson Pike <graysonpike@gmail.com>
2020-12-09 22:53:55 -0600

    Add remaining(), get_duration(), is_active(), etc. to Timer

--- game/timer.cpp
+++ game/timer.cpp
@@ -1,22 +1,60 @@
 #include "timer.h"
 #include <SDL2/SDL.h>
 
-Timer::Timer(float duration) {
 
-	this->duration = (unsigned)(duration * 1000); 
-	reset();
+Timer::Timer() : duration(0) {}
+
+
+Timer::Timer(float duration) {
+    this->duration = (unsigned)(duration * 1000);
+}
+
+
+void Timer::update() {
+    
+    if (active) {
+        unsigned int current_time = SDL_GetTicks();
+        active = !(current_time > start_time + duration);
+    }
 
 }
+
 
 void Timer::reset() {
 
-	start_time = SDL_GetTicks();
+    start_time = SDL_GetTicks();
+    active = true;
 
 }
 
+
 bool Timer::is_done() {
 
-	unsigned int current_time = SDL_GetTicks();
-	return current_time > (start_time + duration);
+    update();
+    return !active;
 
+}
+
+
+bool Timer::is_active() {
+
+    update();
+    return active;
+    
+}
+
+
+float Timer::remaining() {
+
+    update();
+    if (!active) {
+        return 0.0f;
+    }
+    return 1 - (SDL_GetTicks() - start_time) / (float)duration;
+
+}
+
+
+float Timer::get_duration() {
+    return duration;
 }
--- game/timer.h
+++ game/timer.h
@@ -1,16 +1,23 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+
 class Timer {
 
-	unsigned int start_time;
-	unsigned int duration;
+    unsigned int start_time;
+    unsigned int duration;
+    bool active = false;
+    void update();
 
 public:
 
-	Timer(float duration);
-	void reset();
-	bool is_done();
+    Timer();
+    Timer(float duration);
+    void reset();
+    bool is_done();
+    bool is_active();
+    float remaining(); // fraction of time remaining
+    float get_duration();
 
 };
 
