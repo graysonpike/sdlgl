e18e975a2b21218e0413c498cbe6db523d44eb74
Grayson Pike <graysonpike@gmail.com>
2022-02-18 21:55:35 -0500

    Remove unused bitmasks

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -61,20 +61,7 @@ bool Graphics::init_sdl(std::string window_title) {
 }
 
 void Graphics::init_capture_surface() {
-
-    // Account for endianness
-    Uint32 rmask, gmask, bmask, amask;
-    #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-        rmask = 0xff000000;
-        gmask = 0x00ff0000;
-        bmask = 0x0000ff00;
-        amask = 0x000000ff;
-    #else
-        rmask = 0x000000ff;
-        gmask = 0x0000ff00;
-        bmask = 0x00ff0000;
-        amask = 0xff000000;
-    #endif
+    
     capture_surface = SDL_CreateRGBSurface(0, this->width, this->height, 32, 0, 0, 0, 0);
 
 }
