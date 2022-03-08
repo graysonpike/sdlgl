216a9cab0ad93bd242e52100e7e6cf08b5fc4c3b
Grayson Pike <Grayson_Pike@mckinsey.com>
2022-03-07 23:09:32 -0500

    Add vector utility functions

--- utilities/math.cpp
+++ utilities/math.cpp
@@ -13,6 +13,15 @@ std::pair<int, int> Math::rotate_vector(int magnitude, float angle) {
 }
 
 
+std::pair<float, float> Math::rotate_vector(float magnitude, float angle) {
+
+    float x = magnitude * sin(angle);
+    float y = -magnitude * cos(angle);
+    return std::pair<float, float>(x, y);
+    
+}
+
+
 std::pair<float, float> Math::rotate_about_point(std::pair<float, float> center, std::pair<float, float> moved_point, float angle) {
         
     // Normalize to origin before rotation
@@ -30,4 +39,19 @@ std::pair<float, float> Math::rotate_about_point(std::pair<float, float> center,
 
     return new_point;
 
+}
+
+
+float Math::magnitude(std::pair<float, float> vector) {
+    return sqrt(vector.first * vector.first + vector.second * vector.second);
+}
+
+
+std::pair<float, float> Math::add_vectors(std::pair<float, float> a, std::pair<float, float> b) {
+    return std::pair<float, float>(a.first + b.first, a.second + b.second);
+}
+
+
+std::pair<float, float> Math::scale_vector(std::pair<float, float> v, float factor) {
+    return std::pair<float, float>(v.first * factor, v.second * factor);
 }
--- utilities/math.h
+++ utilities/math.h
@@ -14,7 +14,19 @@ public:
     //           rotate_vector(1, M_PI) => (0, 1) 
     static std::pair<int, int> rotate_vector(int magnitude, float angle);
 
+    // Rotate a vector starting straight up (0, -magnitude) clockwise.
+    // Examples: rotate_vector(1, 0) => (0, -1) 
+    //           rotate_vector(1, M_PI/2) => (1, 0) 
+    //           rotate_vector(1, M_PI) => (0, 1) 
+    static std::pair<float, float> rotate_vector(float magnitude, float angle);
+
     static std::pair<float, float> rotate_about_point(std::pair<float, float> center, std::pair<float, float> moved_point, float angle);
+
+    static float magnitude(std::pair<float, float> vector);
+
+    static std::pair<float, float> add_vectors(std::pair<float, float> a, std::pair<float, float> b);
+
+    static std::pair<float, float> scale_vector(std::pair<float, float> v, float factor);
     
 };
 
